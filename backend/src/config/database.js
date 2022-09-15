9e2904711ba5fdd671145415ebe8c938949507b3
new20121439 <tranvandung727602@gmail.com>
2022-09-15 20:08:37 +0700

    fix minor

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm';
 export const datasource = new DataSource({
     type: process.env.DB_TYPE,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: parseInt(process.env.DB_PORT),
     database: process.env.DB_NAME,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    connectTimeoutMS: 3000,
+    // connectTimeoutMS: 3000,
     entities: [
         '**/**.entity.js'
     ],
